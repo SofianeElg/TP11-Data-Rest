242299798c6379ad0693371a279d6902a1f6a8bb
Sofian <sofiane02.elghazzali@gmail.com>
2025-11-08 20:28:52 +0100

    first commit

---
+++ src/main/java/com/example/Data/REST/MsBanqueApplication.java
@@ -0,0 +1,41 @@
+package com.example.Data.REST;
+
+
+import com.example.Data.REST.entities.Client;
+import com.example.Data.REST.entities.Compte;
+import com.example.Data.REST.entities.TypeCompte;
+import com.example.Data.REST.repositories.ClientRepository;
+import com.example.Data.REST.repositories.CompteRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+
+import java.util.Date;
+
+@SpringBootApplication
+public class MsBanqueApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsBanqueApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(CompteRepository compteRepository, ClientRepository clientRepository, RepositoryRestConfiguration restConfiguration){
+        return args -> {
+            restConfiguration.exposeIdsFor(Compte.class);
+
+            Client c1 = clientRepository.save(new Client(null, "sofyan" ,"sofyan@gmail.com",null));
+            Client c2 = clientRepository.save(new Client(null, "Aalla" ,"Aalla@gmail.com" ,null));
+
+            compteRepository.save(new Compte(null, Math.random() * 9000, new Date(), TypeCompte.EPARGNE, c1));
+            compteRepository.save(new Compte(null, Math.random() * 9000, new Date(), TypeCompte.COURANT, c1));
+            compteRepository.save(new Compte(null, Math.random() * 9000, new Date(), TypeCompte.EPARGNE, c2));
+
+            compteRepository.findAll().forEach(c -> {
+                System.out.println(c.toString());
+            });
+        };
+    }
+}
---
+++ src/main/java/com/example/Data/REST/repositories/CompteRepository.java
@@ -0,0 +1,10 @@
+package com.example.Data.REST.repositories;
+
+
+import com.example.Data.REST.entities.Compte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CompteRepository extends JpaRepository<Compte, Long> {
+}
